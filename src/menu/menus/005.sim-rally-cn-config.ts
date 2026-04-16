19c88ca0ae8b3400fc630e9c4bbf366e105f3dc4
xclhove <xclhove@qq.com>
2026-04-16 20:43:07 +0800

    perf(menu): 修改 SimRallyCN 配置菜单图标
    1. 修改 SimRallyCN 配置菜单图标

--- src/menu/menus/005.sim-rally-cn-config.ts
+++ src/menu/menus/005.sim-rally-cn-config.ts
@@ -1,10 +1,10 @@
 import { defineMenu } from '@/menu'
 import { h } from 'vue'
-import { Setting } from '@element-plus/icons-vue'
+import IconSimRallyCn from '@/components/icon-sim-rally-cn/icon-sim-rally-cn.vue'
 
 export default defineMenu(() => {
   return {
     label: 'SimRallyCN 配置',
-    icon: () => h(Setting),
+    icon: () => h(IconSimRallyCn),
   }
 })
